150391d5c8689ba708736dde9d73028116172ac4
Tomasz Kowalik <tomaszkowalik85@gmail.com>
2021-04-15 18:05:35 +0200

    add hash router

--- src/Components/MainPhoto/index.js
+++ src/Components/MainPhoto/index.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import { Card, Col, Row } from 'react-bootstrap';
+import photo from './photo.jpg';
 
 import './style.css';
 
 class MainPhoto extends Component {
  static defaultProps = {
-  photo: '/photo.jpg',
   cardTitle: 'Tomasz Kowalik',
   cardUnderTitle: 'front end developer',
  };
+
  render() {
   return (
    //hides when vw < 992px
    <Card className="d-none d-md-none d-lg-flex Card text-white">
-    <Card.Img className="CardImage" src={this.props.photo} alt="Card image" />
+    <Card.Img className="CardImage" src={photo} alt="Card image" />
     <Card.ImgOverlay className="CardBody">
      <Row>
       <Col>
--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
  <React.StrictMode>
-  <BrowserRouter>
+  <HashRouter>
    <App />
-  </BrowserRouter>
+  </HashRouter>
  </React.StrictMode>,
  document.getElementById('root')
 );
