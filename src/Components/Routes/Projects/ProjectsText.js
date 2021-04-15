1c219e3aa6464b1a5164ed45cf628c8f27541dab
Tomasz Kowalik <tomaszkowalik85@gmail.com>
2021-04-15 23:09:36 +0200

    Updates

--- src/Components/Routes/Projects/ProjectsText.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class ProjectsText extends Component {
- render() {
-  return <span> Tu niebawem będzie content </span>;
- }
-}
-
-export default ProjectsText;
